573026357d38d929b8b01ea437ce188560be08f1
Rivedoom <z.jakub26@gmail.com>
2026-02-11 19:19:20 +0100

    Dodaj pliki projektów.

---
+++ miniblog/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace miniblog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tytuł jest wymagany")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Treść jest wymagana")]
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
